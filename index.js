b75bb11ee11add35c47876517031767586b60466
lachlan <lachlan.miller.1990@outlook.com>
2017-10-27 22:39:15 +0900

    Start work on test flag

--- index.js
+++ index.js
@@ -7,10 +7,22 @@ const path  = process.argv.slice(2)[0]
 
 const child = exec('touch ' + path, (err, stdout, stderr) => {
   if (err) console.log(err)
-  const name = utils.getFilename(path)
-  const options = utils.getOptionsByName(argv.options)
-  const nameWithoutExt = utils.removeExtension(name)
+
+  let options
+  let name
+  let nameWithoutExt
+  console.log('args', argv)
+  name = utils.getFilename(path)
+  if (argv.options)
+    options = utils.getOptionsByName(argv.options)
+
+  if (argv.t) 
+    utils.generateTest()
+
+  nameWithoutExt = utils.removeExtension(name)
+  
+
   fs.writeFile(path, utils.template(nameWithoutExt, options), (err) => {
     if (err) console.log(err)
-  })
+  }) 
 })
