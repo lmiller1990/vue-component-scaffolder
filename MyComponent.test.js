59b7d65deb382b45ba108139107a85b81d985736
lachlan <lachlan.miller.1990@outlook.com>
2018-01-09 19:07:51 +0900

    Add ability to pass -t and generated test

--- MyComponent.test.js
+++
@@ -1,8 +0,0 @@
-import { shallow } from 'vue-test-utils'
-import MyComponent from './MyComponent'
-
-describe(MyComponent, () => {
-  it('renders', () => {
-    const wrapper = shallow(MyComponent)
-  })
-})
--- index.js
+++ index.js
@@ -4,61 +4,33 @@ const utils =  require('./utils')
 const exec  = require('child_process').exec
 const argv  = require('minimist')(process.argv.slice(2))
 const path  = process.argv.slice(2)[0]
-  /*
-const child = exec('touch ' + path, (err, stdout, stderr) => {
-  if (err) {
-    console.log(err)
-  }
 
-  console.log(argv)
-  let options
-  let name
-  let nameWithoutExt
-
-  name = utils.getFilename(path)
-  if (argv.options) {
-    options = utils.getOptionsByName(argv.options)
-  }
-
-  nameWithoutExt = utils.removeExtension(name)
-  options = argv.options !== undefined ? utils.getOptionsByName(argv.options) : undefined
-  nameWithoutExt = utils.removeExtension(name)
-
-  fs.writeFile(path, utils.template(nameWithoutExt, options), (err) => {
-    if (err) {
-      console.log(err)
-    } 
-  }) 
-
-  if (argv.t) {
-    console.log('test!')
-    fs.writeFile(path, utils.testTemplate(nameWithoutExt), (err) => {
-      if (err) console.log(err)
-    }) 
-  }
-})*/
-
-const writeToFile = (path, templateFn, ext) => {
+const writeToFile = (path, templateFn, ext, options = null) => {
   path = path + ext
   const child = exec('touch ' + path, (err, stdout, stderr) => {
     if (err) {
       console.log(err)
     }
 
+    let opts
     let name
     let nameWithoutExt
+
+    if (options) {
+      opts = utils.getOptionsByName(argv.options)
+    }
 
     name = utils.getFilename(path)
     nameWithoutExt = utils.removeExtension(name)
 
-    fs.writeFile(path, templateFn(nameWithoutExt), (err) => {
+    fs.writeFile(path, templateFn(nameWithoutExt, opts), (err) => {
       if (err) console.log(err)
     }) 
   })
 }
 
-writeToFile(path, utils.template, '.vue')
+writeToFile(path, utils.template, '.vue', argv.options)
 
 if (argv.t) {
-  writeToFile(path, utils.testTemplate, '.test.js')
+  writeToFile(path, utils.testTemplate, '.test.js', argv.options)
 }
